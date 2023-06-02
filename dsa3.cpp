0155ea65dbb743a0bc6122b3606e9291264cd393
Ridhima Sinare <97884043+RidhimaSinare@users.noreply.github.com>
2023-06-02 20:07:09 +0530

    Update dsa3.cpp

--- dsa3.cpp
+++ dsa3.cpp
@@ -7,15 +7,18 @@ class node
     int data;
     node *left,*right;
     bool lThread,rThread;
-
-    public:
+public:
+    node()
+    {
+        data=0;
+        left=right=nullptr;
+        lThread=rThread=true;
+    }
     node(int d)
     {
-        this->data=d;
-        this->left=nullptr;
-        this->right=nullptr;
-        this->lThread=true;
-        this->rThread=true;
+        data=d;
+        left=right=nullptr;
+        lThread=rThread=true;
     }
 
     friend class TBST;
@@ -24,299 +27,297 @@ class node
 class TBST
 {
     node *root;
-    public:
+public:
     TBST()
     {
         root=nullptr;
-        cout<<endl<<"Creation of Threaded BST";
-        cout<<endl<<"Enter number of nodes: ";
-        int n;cin>>n;
-        cout<<"Enter value of each node: ";
-        int a[n];
-        for(int i=0;i<n;i++)
-        {
-            cin>>a[i];
-            insert(a[i]);
-        }
-
     }
 
-    void insert(int key);
-    void inorder();void preorder();void postorder();
-    node *leftmost(node *root);
-    node *inorderSucc(node *root);
-    node *inorderPred(node *root);
-    void deletion(int key);
+    void insert()
+    {   int data;
+        if(root==nullptr)
+        {
+            cout<<endl<<"Enter root value: ";cin>>data;
+            node *newNode=new node(data);
+            root=newNode;
+            return ;
+        }
+        else
+        {
+            cout<<endl<<"Enter key to insert: ";cin>>data;
+            node *newNode=new node(data);
+            node *curr=root;
+            node *parent=nullptr;
+
+            while(curr!=nullptr)
+            {
+                
+                if(data==curr->data)
+                {
+                    cout<<endl<<"key exists";
+                    return;
+                }
+                
+                parent=curr;
+                if(curr->data > data)
+                {
+                    if(curr->lThread==false)    //left child exists
+                    {
+                        curr=curr->left;
+                    }
+                    else{break;}
+                }
+                else
+                {
+                    if(curr->rThread==false)    //right child present
+                    {
+                        curr=curr->right;
+                    }
+                    else{break;}
+                }
+            }//end of while parent=curr has happened and now connect newNode and parent
+
+            if((data<parent->data))   //left child of parent
+            {
+                newNode->left=parent->left;
+                parent->left=newNode;
+                newNode->right=parent;
+                //newNode->lThread=true;
+                parent->lThread=false;
+                cout<<endl<<"Inserted successfully";
+            }
+            else
+            {
+                newNode->right=parent->right;
+                parent->right=newNode;
+                newNode->left=parent;
+                //newNode->lThread=newNode->rThread=true;
+                parent->rThread=false;
+                cout<<endl<<"Inserted successfully";
+            }
+
+        }
+    }//end of insert
+
+    void inorder()
+    {
+        node *curr=root;
+
+        while(curr->left!=nullptr)
+        {
+            curr=curr->left;    //reached smallest and leftmost key
+        }
+        cout<<endl;
+        while(curr!=nullptr)
+        {
+            cout<<curr->data<<"  ";
+
+            if(curr -> left == nullptr && curr -> right == nullptr){    //for only one key ie root
+                cout<<endl;
+                return;
+            }
+
+            if(curr->rThread==true)
+            {
+                curr=curr->right;
+            }
+            else if(curr->right==nullptr)
+            {
+                return ;
+            }
+            else
+            {
+                curr=curr->right;
+                while(curr->lThread==false)     //IMP CONDITION  CHECK THREAD NOT CHILD
+                {
+                    curr=curr->left;
+                }
+            }
+        }
+    }// end of inorder
+
+    void preorder()
+    {
+        node *curr=root;
+        cout<<endl;
+        while(curr!=nullptr)
+        {
+            cout<<curr->data<<"  ";
+            
+            if(curr->lThread==false)
+            {
+                curr=curr->left;
+            }
+            else if(curr->rThread==false)
+            {
+                curr=curr->right;
+            }
+            else
+            {
+                while(curr!=nullptr && curr->rThread==true)
+                {
+                    curr=curr->right;
+                }
+                if(curr!=nullptr)
+                {
+                    curr=curr->right;
+                }
+            }
+        }
+    }//end of preorder
+
+    void deletion(int key)
+    {   //out<<"\nentering deletion";
+        node *curr=root;
+        node *parent=nullptr;
+        while(curr!=nullptr)
+        {
+            if(key==curr->data)
+            {
+                break;      //IMP USE BREAK AND NOT RETURN
+            }
+            parent =curr;
+            if(key<curr->data)
+            {
+                if(curr->lThread==false)
+                {
+                curr=curr->left;}
+                else{break;}
+            }
+            else
+            {   if(curr->rThread==false){
+                curr=curr->right;}
+                else{break;}
+            }
+        }   //fount the element or reached the end
+
+        if(curr->data!=key)
+        {
+            cout<<endl<<"Element does not exist";
+            return ;
+        }
+        //leaf node
+        else if(curr->lThread==true && curr->rThread==true)
+        {
+    
+            if(parent==nullptr)
+                root=nullptr;
+            else if(parent->left==curr)
+            {
+                parent->left=curr->left;
+                parent->lThread=true;
+            }
+            else
+            {
+                parent->right=curr->right;
+                parent->rThread=true;
+            }
+            delete curr;
+            cout<<endl<<"Deletion no child successful";
+            return;
+        }
+        //to be deleted node has 1 child
+        else if((curr->lThread==true && curr->rThread==false)||(curr->lThread==false && curr->rThread==true))
+        {   node *child;
+            //has left child
+            if(curr->lThread==false)
+            {
+                child=curr->left;
+            }
+            else
+            {
+                child=curr->right;
+            }//got the child of to be deleted node
+
+            //check if deleted node is left/r child
+            
+            if(parent==nullptr)
+            {
+                root=nullptr;
+                return;
+            }
+            else if(curr==parent->left)
+            {
+                parent->left=child;
+            }
+            else
+            {
+                parent->right=child;
+            }
+            node *s=inordersucc(curr);
+            node *p=pred(curr);
+
+            if(curr->lThread==false)
+            {
+                p->right=s;
+            }
+            else
+            {
+                s->left=p;
+            }
+            delete curr;
+            cout<<endl<<"Deletion 1 child successful";
+            return ;
+
+        }
+    //2 child
+        else
+        {
+            node *s=inordersucc(curr);
+            int t=s->data;
+            deletion(s->data);  //IMP FIRST DELETION 
+            curr->data=t;       //THEN REALLOCATION
+            cout<<endl<<"deletion 2 child";
+            return ;
+        }
+    }//end of deletion
+
+    node *inordersucc(node *temp)
+    {
+        if(temp->rThread==true)
+            return temp->right;
+        else
+        {
+            temp=temp->right;
+            while(temp->left!=nullptr && temp->lThread==false)  //IMP TO REMEMBER BOTH CONDITIONS
+            {
+                temp=temp->left;
+            }
+            return temp;
+        }
+    }
+
+    node *pred(node *temp)
+    {
+        return temp->left;
+    }
+
+
 };
 
-void TBST::insert(int key)
-{
-    node *newNode=new node(key);
-    if(root==nullptr)
-    {
-        root=newNode;
-        return;
-    }
-
-    node *temp=root;
-    node *parent=nullptr;
-
-    while(temp!=nullptr)
-    {
-        if(key==temp->data)
-            cout<<endl<<"Key already exists";
-            return;
-        parent=temp;
-        if(key<temp->data)
-        {
-            if(temp->lThread==false)//thread not present, child present
-                temp-temp->left;
-            else    
-                break;
-        }
-        else
-        {
-            if(temp->rThread==false)
-                temp=temp->right;
-            else
-                break;
-        }
-    }
-
-    if(key<temp->data)
-    {
-        newNode->left=parent->left;
-        newNode->right=parent;
-        parent->lThread=false;  //child present , thread removed
-        parent->left=newNode;
-    }
-    else
-    {
-        newNode->right=parent->right;
-        newNode->left=parent;
-        parent->rThread=false;
-        parent->right=newNode;
-    }
-}
-
-void TBST::deletion(int key)
-{
-    node *temp=root;
-    node *parent=nullptr;
-
-    while(temp!=nullptr)
-    {
-        if(key==temp->data)
-            return;
-        parent=temp;
-        if(key<temp->data)
-            if(temp->lThread==false) temp=temp->left;
-            else break;
-        else
-            if(temp->rThread==false) temp=temp->right;
-            else break;
-    }
-    if(temp->data!=key)
-        cout<<endl<<"Key not found";
-
-    //deleting leaf node
-    else if(temp->lThread==true && temp->rThread==true)
-    {
-        if(parent=nullptr)
-            root=nullptr;
-        else if(temp==parent->left)
-           { parent->lThread=true;
-            parent->left=temp->left;}
-        else
-            parent->rThread=true;
-            parent->right=temp->right;
-
-        delete temp;
-        cout<<endl<<"Node deleted successfully";
-        return;
-    }
-    //deleting node with single child
-    else if((temp->lThread==false && temp->rThread==true)||(temp->rThread==true && temp->rThread==false))
-    {
-        node *child;
-        if(temp->lThread==false)
-            child=temp->left;
-        else
-            child=temp->right;
-        
-        if(parent==nullptr)
-            root=child;
-        else if(temp=parent->left)
-        {
-            parent->left=child;
-        }
-        else
-            parent->right=child;
-
-        node *s=inorderSucc(temp);
-        node *p=inorderPred(temp);
-
-        if(temp->lThread==false)    //left child present
-            p->right=s;
-        else
-            s->left=p;
-
-        delete temp;
-        cout<<endl<<"Node deleted successfully";
-        return;
-    }
-
-    //deleting node with 2 children
-    else
-    {
-        node *s=inorderSucc(temp);
-        int t=s->data;
-        deletion(s->data);
-        temp->data=t;
-    }
-
-    
-}// end of deletion
-
-node* TBST::leftmost(node *temp)
-{
-    if(temp==nullptr)
-        return nullptr;
-    while(temp->left!=nullptr && temp->lThread==false)
-    {
-        temp=temp->left;
-    }
-    return temp;
-}
-
-node* TBST::inorderSucc(node *temp)
-{
-    if(temp->rThread==true)
-        return temp->right;
-    else
-        temp=temp->right;
-        return leftmost(temp);
-}
-
-node* TBST::inorderPred(node *temp)
-{
-    return temp->left;
-}
-
-void TBST::inorder()
-{
-    node *temp=leftmost(root);
-    cout<<endl;
-    while(temp!=nullptr)
-    {
-        cout<<temp->data<<"  ";
-        if(temp->rThread==true) temp=temp->right;
-        else{
-            temp=temp->right;
-            temp=leftmost(temp);
-        }
-    }
-}
-
-void TBST::preorder()
-{
-    node *temp=root;
-    cout<<endl;
-    while(temp!=nullptr)
-    {
-        cout<<temp->data<<"  ";
-        if(temp->lThread==false)
-        {
-            temp=temp->left;
-        }
-        else if(temp->rThread==false)
-        {
-            temp=temp->right;
-        }
-        else
-        {
-            while(temp!=nullptr && temp->rThread==true)
-            {
-                temp=temp->right;
-            }
-            if(temp!=nullptr)
-                temp=temp->right;
-        }
-
-    }
-    cout<<endl;
-}
-
-void TBST::postorder()
-{
-    stack <node*>s;
-    stack <node*> out;
-    node *temp=root;
-    s.push(temp);
-    while(!s.empty())
-    {
-        temp=s.top();
-        s.pop();
-        out.push(temp);
-        if(temp->lThread==false)
-            s.push(temp->left);
-        if(temp->rThread==false)
-            s.push(temp->right);
-
-    }
-    while(!out.empty())
-    {
-        temp=out.top();
-        out.pop();
-        cout<<temp->data<<" ";
-    }
-    cout<<endl;
-}
 
 int main()
 {
-	TBST tree;
-	while(true)
+    TBST t;
+    int ch;
+
+    while(true)
     {
-		cout<<"Implementation of Threaded BST"<<endl<<endl;
-		cout<<"1. Traversal of Threaded BST"<<endl;
-		cout<<"2. Delete a node from Threaded BST"<<endl;
-		cout<<"3. Exit"<<endl;
-		int res;
-		cout<<"Enter your choice : ";
-		cin>>res;
-		if(res==1)
-        {
-			cout<<"Inorder :";
-			tree.inorder();
-			cout<<"Preorder :";
-			tree.preorder();
-			cout<<"Postorder :";
-			tree.postorder();
-			cout<<endl;
-		}
-		else if(res==2)
-        {
-			int n;
-			cout<<"Enter a no. to delete from threaded BST : ";
-			cin>>n;
-			tree.deletion(n);
-			cout<<"After deletion : "<<endl;
-			cout<<"Inorder :";
-			tree.inorder();
-			cout<<"Preorder :";
-			tree.preorder();
-			cout<<"Postorder :";
-			tree.postorder();
-			cout<<endl;
-		}
-		else if(res==3)
-        {
-			cout<<"Terminating program"<<endl;
-			break;
-		}
-		else
-            continue;
-	}
-	return 0;
+        cout<<endl<<"1.Insert\n2.Display Inorder\n3.Display Preorder\n4.Delet\n5.Exit";
+        cout<<endl<<"Enter choice: ";cin>>ch;
+        switch (ch)
+        {case 1:t.insert();
+                break;
+        case 2:t.inorder();break;
+        case 3:t.preorder();break;
+        case 4:
+                int key;
+                cout<<endl<<"Enter number to delete: ";cin>>key;
+                t.deletion(key);break;
+        case 5:exit(0);
+        
+        default:
+            break;
+        }
+    }
 }
